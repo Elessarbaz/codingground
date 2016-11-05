df8863171f7dae3998d59b78b041136e49e86567
Elessarbaz <darmen89@yandex.ru>
2016-11-05 22:06:00 +0600

    Update main.pas

--- New Project-20160929/main.pas
+++ New Project-20160929/main.pas
@@ -1,4 +1,13 @@
-Program HelloWorld(output);
+var c, l : integer;
+var number, fnumber : string;
+
 begin
-  writeln('Hello, world!');
+    readln(number);
+    
+    for c:=1 to length(number) do begin
+        fnumber := fnumber + number[c];
+        if (c mod 3 = 0) and (c < length(number))
+            then fnumber := fnumber + '-';
+    end;
+    writeln(fnumber);
 end.
